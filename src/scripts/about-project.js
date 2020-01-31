1eabfba827b0b07bf7b6e91fa64d1ce609376722
Eugene Tsy <tomakesence@gmail.com>
2020-02-01 00:40:18 +0300

    Доработана первая версия инфраструктуры и верстки

--- src/scripts/about-project.js
+++ src/scripts/about-project.js
@@ -1,17 +1,10 @@
 import "../css/about-project.css";
 import "../../node_modules/@glidejs/glide/dist/css/glide.core.css";
+import "../../node_modules/@glidejs/glide/dist/css/glide.theme.css";
 
-import Glide from '@glidejs/glide';
+import "./slider.js";
 
-const firstGlide = new Glide('.glide', {
-  type: 'carousel',
-  startAt: 'center',
-  peek: 88,
-  gap: 16,
-  startAt: 1,
-  perView: 4,
-})
-.mount()
+
 
 
 
--- src/scripts/analytics.js
+++ src/scripts/analytics.js
@@ -1,2 +1,2 @@
-import '../css/analytics.css';
+import '../css/analytics-page.css';
 
